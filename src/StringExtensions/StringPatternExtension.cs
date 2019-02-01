e3429b00e0b4c895f43484c8ec88c5199baccc75
Sangwoo Oh <soh@lampsplus.com>
2019-02-01 13:28:31 -0800

    Update src/StringExtensions/StringPatternExtension.cs
    some code addition

--- src/StringExtensions/StringPatternExtension.cs
+++ src/StringExtensions/StringPatternExtension.cs
@@ -1,23 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace StringExtensions
 {
     public static class StringPatternExtension
     {
-        public static bool IsMatchedWith(this string value, string pattern)
+        private static readonly Regex TokensRegex = new Regex(@"\\\{(\w+)\}", RegexOptions.Compiled);
+
+        public static bool IsMatchedWith(this string value, string pattern, bool isCaseSensitive = false)
         {
-            var patternTokens = pattern.Split(new[] { '{', '}' });
+            var patternTokens = pattern.Split('{', '}');
             var cursor = 0;
-            for(var i = 0; i < patternTokens.Length; i+=2)
+            for (var i = 0; i < patternTokens.Length; i += 2)
             {
                 if (string.IsNullOrEmpty(patternTokens[i])) continue;
 
-                cursor = value.IndexOf(patternTokens[i], cursor);
+                cursor = value.IndexOf(patternTokens[i], cursor, isCaseSensitive ? StringComparison.InvariantCulture : StringComparison.CurrentCultureIgnoreCase);
                 if (cursor < 0) return false;
                 cursor += patternTokens[i].Length;
             }
 
             return true;
         }
+
+        //public static Dictionary<string, string> GetTokensWith(this string value, string pattern, bool isCaseSensitive = false)
+        //{
+        //    var matches = new Regex(TokensRegex.Replace(Regex.Escape(pattern), match => $"(?<{match.Groups[1].Value}>\\w+)")).Match(value);
+
+        //    return matches.Groups
+        //        .Select(g => new { name = g.Name, value = g.Value })
+        //        .ToDictionary(o => o.name, o => o.value);
+        //}
+
+        //public static string FormatWith(this string pattern, params object[] values)
+        //{
+        //    var replaced = TokensRegex.Replace(Regex.Escape(pattern), match => $"{{{match.Value}}}");
+        //    return Regex.Unescape(string.Format(replaced, values));
+        //}
     }
 }
